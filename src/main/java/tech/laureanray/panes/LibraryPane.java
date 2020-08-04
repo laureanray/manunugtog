0dc3d14d8d8a850845442ef5c9e6525ad8385a54
laureanray <laureanraybahala@gmail.com>
2020-08-05 03:27:10 +0800

    :heavy_plus_sign: moved library view to separate class

--- src/main/java/tech/laureanray/panes/LibraryPane.java
+++ src/main/java/tech/laureanray/panes/LibraryPane.java
@@ -1,5 +1,25 @@
 package tech.laureanray.panes;
 
-public class LibraryPane {
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
 
+public class LibraryPane extends HBox {
+    private static LibraryPane instance;
+
+    private LibraryPane() {
+    }
+
+    private static void initialize() {
+        Text text = new Text("LibraryPane");
+        instance.getChildren().add(text);
+    }
+
+    public static LibraryPane getInstance() {
+        if (instance == null) {
+            instance = new LibraryPane();
+            initialize();
+        }
+
+        return instance;
+    }
 }
--- src/main/java/tech/laureanray/ui/ViewSwitcher.java
+++ src/main/java/tech/laureanray/ui/ViewSwitcher.java
@@ -3,6 +3,7 @@ package tech.laureanray.ui;
 import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
 import javafx.scene.text.Text;
+import tech.laureanray.panes.LibraryPane;
 
 public class ViewSwitcher extends HBox  {
     private static ViewSwitcher instance;
@@ -17,11 +18,11 @@ public class ViewSwitcher extends HBox  {
         Button albums = new Button("Albums");
         songs.setId("b1");
 
-        HBox songsView = new HBox(new Text("Songs"));
+        HBox libraryPane = LibraryPane.getInstance();
         HBox albumsView = new HBox(new Text("Albums"));
 
         songs.setOnMouseClicked(e -> {
-            MainScene.getMainPane().setCenter(songsView);
+            MainScene.getMainPane().setCenter(libraryPane);
         });
 
         albums.setOnMouseClicked(e -> {
@@ -33,7 +34,7 @@ public class ViewSwitcher extends HBox  {
         this.getChildren().add(songs);
         this.getChildren().add(albums);
 
-        MainScene.getMainPane().setCenter(songsView);
+        MainScene.getMainPane().setCenter(libraryPane);
     }
 
     public static ViewSwitcher getInstance() {
