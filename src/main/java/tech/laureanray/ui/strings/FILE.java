73805bbba3a38a8080c660bd7a8c7563fd3930d9
laureanray <laureanraybahala@gmail.com>
2020-08-16 11:55:24 +0800

    :poop: add FILE

---
+++ src/main/java/tech/laureanray/ui/strings/FILE.java
@@ -0,0 +1,14 @@
+/**
+ * FILE
+ *
+ * @author Laurean Ray
+ * @version 1.0
+ * @since 8/16/20
+ */
+
+package tech.laureanray.ui.strings;
+
+public class FILE {
+    public static final String APP_CONFIG = "application-config.json";
+    public static final String APP_DATA = "application-data.json";
+}
